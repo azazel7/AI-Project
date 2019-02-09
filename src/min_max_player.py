b79432f7faf904da644fb726fa74ee6618960c3c
Martinou <martin.khannouz@gmail.com>
2019-02-09 15:10:00 -0400

    Add many things in min max. Mainly there history of move may be rollback later.

--- src/min_max_player.py
+++ src/min_max_player.py
@@ -26,23 +26,29 @@ class MinMaxPlayer:
         self.depth = int(depth)
         self.sort_moves = sort_moves
 
-    @timeit
+    # @timeit
     def play(self, id_ai, engine):
         self.id_ai = id_ai
         local_engine = copy.deepcopy(engine)
         self.average_number_of_moves = 0
         self.number_of_state = 0
         self.heuristic_called_count = 0
-        move = self.max(id_ai, engine, self.depth, -1000000, 1000000)
+        self.current_best_val = -1000000000
+        self.current_best_mov = []
+        move = self.max(id_ai, engine, self.depth, -100000000, 100000000)
         # print("Visited states: ", self.number_of_state)
         # print("Average moves: ", (self.average_number_of_moves / self.number_of_state))
         # print("Heuristic called: ", self.heuristic_called_count)
-        print("Value (", self.heuristic_object.name, ") = ", move[0])
-        return move[1]
+        # print("Value (", self.heuristic_object.name, ") = ", move[0])
+        for mv in move[1]:
+            mv.print_as_input()
+        print(move[1])
+        return move[1][0]
 
     def heuristic(self, id_ai, engine):
         self.heuristic_called_count += 1
-        return self.heuristic_object.value(id_ai, engine)
+        val = self.heuristic_object.value(id_ai, engine)
+        return val
 
     def value_move(self, engine, move, id_ai):
         engine.do_move(move) #No need to check if the move is legal it has already been done
@@ -55,26 +61,32 @@ class MinMaxPlayer:
             current_val = self.heuristic(self.id_ai, engine)
             return (current_val, None)
         moves = engine.available_moves()
-
+        s = sum([mv.recycling for mv in moves])
         if self.sort_moves:
             value = [self.value_move(engine, mv, self.id_ai) for mv in moves]
-            moves = [mv[1] for mv in sorted(value, key=itemgetter(0), reverse=True)]
+            moves = [mv[1] for mv in sorted(value, key=itemgetter(0), reverse=True) if mv[0] > -900000 ]
 
         self.number_of_state += 1
         self.average_number_of_moves += len(moves)
         best_val = -100000
         best_move = None
+        l_move = []
         for move in moves:
             engine.do_move(move) #No need to check if the move is legal it has already been done
-            value = self.min((id_ai+1)%2, engine, depth-1, alpha, beta)
+            vlue = self.min((id_ai+1)%2, engine, depth-1, alpha, beta)
             engine.cancel_last_move()
-            value = value[0] #Min return tuple
+            value = vlue[0] #Min return tuple
             if value > alpha:
+                l_move = [copy.deepcopy(move)]
+                if vlue[1] is not None:
+                    l_move.extend(vlue[1])
                 alpha = value
                 best_move = move
             if value >= beta:
-                return (value, move)
-        return (alpha, best_move)
+                # return (value, move)
+                return (value, l_move)
+        # return (alpha, best_move)
+        return (alpha, l_move)
 
     def min(self, id_ai, engine, depth, alpha, beta):
         if depth == 0:
@@ -84,19 +96,31 @@ class MinMaxPlayer:
 
         if self.sort_moves:
             value = [self.value_move(engine, mv, self.id_ai) for mv in moves]
-            moves = [mv[1] for mv in sorted(value, key=itemgetter(0))]
+            # moves = [mv[1] for mv in sorted(value, key=itemgetter(0))]
+            best_opponent = min([mv[0] for mv in value])
+            if best_opponent <= -900000:
+                return (best_opponent, None)
+            moves = [mv[1] for mv in sorted(value, key=itemgetter(0), reverse=True)]
+
 
         self.number_of_state += 1
         self.average_number_of_moves += len(moves)
         best_move = None
+        l_move = []
         for move in moves:
             engine.do_move(move) #No need to check if the move is legal it has already been done
-            value = self.max((id_ai+1)%2, engine, depth-1, alpha, beta)
+            vlue = self.max((id_ai+1)%2, engine, depth-1, alpha, beta)
             engine.cancel_last_move()
-            value = value[0] #Min return tuple
+            value = vlue[0] #Min return tuple
             if value < beta:
+                # print(move)
+                l_move = [copy.deepcopy(move)]
+                if vlue[1] is not None:
+                    l_move.extend(vlue[1])
                 beta = value
                 best_move = move
             if value <= alpha:
-                return (value, move)
-        return (beta, best_move)
+                # return (value, move)
+                return (value, l_move)
+        # return (beta, best_move)
+        return (beta, l_move)
