57a3a946cebf41241785f3701ce579364988b346
Martinou <martin.khannouz@gmail.com>
2019-02-08 20:35:43 -0400

    Fix a bug in heuristic dark magic. In addition, valuable spaces are counted in a temporary line.

--- lib/hello.c
+++ lib/hello.c
@@ -89,13 +89,109 @@ static PyObject* histogram(PyObject *dummy, PyObject *args)
 	return PyInt_FromLong(sum);
 }
 
+static PyObject* check_pos(PyObject *dummy, PyObject *args)
+{
+	PyObject *arg_placement_pos=NULL, *arg_no_empty=NULL, *arg_empty=NULL, *arg_recycling_pos=NULL, *arg_board=NULL;
+	PyObject *npy_placement_pos=NULL, *npy_no_empty=NULL, *npy_empty=NULL, *npy_recycling_pos=NULL, *npy_board=NULL;
+	int width, height;
+	int nd;
+
+	if (!PyArg_ParseTuple(args, "OOOOOii", &arg_board, &arg_placement_pos, &arg_no_empty, &arg_empty, &arg_recycling_pos, &width, &height))
+		return NULL;
+
+	npy_board = PyArray_FROM_OTF(arg_board, NPY_DOUBLE, NPY_IN_ARRAY);
+	npy_placement_pos = PyArray_FROM_OTF(arg_placement_pos, NPY_DOUBLE, NPY_IN_ARRAY);
+	npy_no_empty = PyArray_FROM_OTF(arg_no_empty, NPY_DOUBLE, NPY_IN_ARRAY);
+	npy_empty = PyArray_FROM_OTF(arg_empty, NPY_DOUBLE, NPY_IN_ARRAY);
+	npy_recycling_pos = PyArray_FROM_OTF(arg_recycling_pos, NPY_DOUBLE, NPY_IN_ARRAY);
+
+	double const* board = PyArray_DATA(npy_board);
+	double const* placement_pos = PyArray_DATA(npy_placement_pos);
+	double const* no_empty = PyArray_DATA(npy_no_empty);
+	double const* empty = PyArray_DATA(npy_empty);
+	double const* recycling_pos = PyArray_DATA(npy_recycling_pos);
+
+	npy_intp *shape_board = PyArray_SHAPE(npy_board);
+	npy_intp *shape_placement_pos = PyArray_SHAPE(npy_placement_pos);
+	npy_intp *shape_no_empty = PyArray_SHAPE(npy_no_empty);
+	npy_intp *shape_empty = PyArray_SHAPE(npy_empty);
+	npy_intp *shape_recycling_pos = PyArray_SHAPE(npy_recycling_pos);
+
+	int return_value = 1;
+	for(int i = 0; i < shape_placement_pos[0] && return_value == 1; ++i){
+		int x = (int)placement_pos[i*shape_placement_pos[1]];
+		int y = (int)placement_pos[i*shape_placement_pos[1] + 1];
+		if(x < 0 || x >= width || y < 0 || y >= height){
+			return_value = 0;
+			break;
+		}
+		int idx = x*shape_board[1] + y;
+		int val = (int)board[idx];
+		int in_recycling = 0;
+		//Set in_recycling if [x,y] is in recycling_pos
+		for(int j = 0; j < shape_recycling_pos[0]; ++j){
+			int x_rec = (int)recycling_pos[j*shape_recycling_pos[1]];
+			int y_rec = (int)recycling_pos[j*shape_recycling_pos[1] + 1];
+			if(x_rec == x && y_rec == y){
+				in_recycling = 1;
+				break;
+			}
+		}
+		if(val != 0 && !in_recycling){
+			return_value = 0;
+			break;
+		}
+		//TODO check recycling pos
+	}
+	for(int i = 0; i < shape_no_empty[0] && return_value == 1; ++i){
+		int x = (int)no_empty[i*shape_no_empty[1]];
+		int y = (int)no_empty[i*shape_no_empty[1] + 1];
+		if(x >= 0 && x < width && y >= 0 && y < height){
+			int idx = x*shape_board[1] + y;
+			int val = (int)board[idx];
+			int in_recycling = 0;
+			//Set in_recycling if [x,y] is in recycling_pos
+			for(int j = 0; j < shape_recycling_pos[0]; ++j){
+				int x_rec = (int)recycling_pos[j*shape_recycling_pos[1]];
+				int y_rec = (int)recycling_pos[j*shape_recycling_pos[1] + 1];
+				if(x_rec == x && y_rec == y){
+					in_recycling = 1;
+					break;
+				}
+			}
+			if(val == 0 || in_recycling){
+				return_value = 0;
+				break;
+			}
+		}
+	}
+	for(int i = 0; i < shape_empty[0] && return_value == 1; ++i){
+		int x = (int)empty[i*shape_empty[1]];
+		int y = (int)empty[i*shape_empty[1] + 1];
+		if(x >= 0 && x < width && y >= 0 && y < height){
+			int idx = x*shape_board[1] + y;
+			int val = (int)board[idx];
+			if(val != 0){
+				return_value = 0;
+				break;
+			}
+		}
+	}
+    Py_DECREF(npy_board);
+    Py_DECREF(npy_placement_pos);
+    Py_DECREF(npy_no_empty);
+    Py_DECREF(npy_empty);
+    Py_DECREF(npy_recycling_pos);
+	return PyInt_FromLong(return_value);
+}
+
 static PyObject* check_move(PyObject *dummy, PyObject *args)
 {
 	PyObject *arg_board=NULL;
 	PyObject *npy_board=NULL;
 	int type, recycling, x_move, y_move, x_rec, y_rec, card_to_rec;
 
-	if (!PyArg_ParseTuple(args, "Oiiiiiii", &arg_board, &recycling, &type, &x_move, &y_move, &x_rec, &y_rec, &card_to_rec))
+	if (!PyArg_ParseTuple(args, "Opiiiiii", &arg_board, &recycling, &type, &x_move, &y_move, &x_rec, &y_rec, &card_to_rec))
 		return NULL;
 
 	npy_board = PyArray_FROM_OTF(arg_board, NPY_DOUBLE, NPY_IN_ARRAY);
@@ -223,7 +319,7 @@ static PyObject* check_move(PyObject *dummy, PyObject *args)
 	return PyInt_FromLong(return_value);
 }
 
-static void count_line(int const x_start,
+inline void count_line(int const x_start,
 					   int const y_start,
  					   int const x_offset_mul,
 					   int const y_offset_mul,
@@ -235,14 +331,22 @@ static void count_line(int const x_start,
 	int offset = 0;
 
 	int prev_val = -1;
-	int prev_space = -1;
+	int prev_offset[2] = {-1, -1};
 	int count[2];
+#define safe_size_temp_line (12+8)
+	int tmp_vspaces[2][safe_size_temp_line]; //12+8 is just to be sure we have enough space
+	for(int i = 0; i < 2; ++i)
+		for(int j = 0; j < safe_size_temp_line; ++j)
+			tmp_vspaces[i][j] = 0;
 	do{	
+		//Compute the current x and y
 		int x = x_start + offset*x_offset_mul;
 		int y = y_start + offset*y_offset_mul;
+		//Check if this position is on the board
 		if(x < 0 || x >= shape_board[0] || y < 0 || y >= shape_board[1]){
 			break;
 		}
+		//Get the real index and the current value
 		int idx = x*shape_board[1] + y;
 		int val = board[idx];
 		/*First turn, we need to initialize count*/
@@ -251,7 +355,7 @@ static void count_line(int const x_start,
 				count[0] = count[1] = 1;
 			else{
 				count[0] = count[1] = 0;
-				prev_space = idx;
+				prev_offset[0] = prev_offset[1] = offset;
 			}
 		}
 		else{
@@ -260,15 +364,17 @@ static void count_line(int const x_start,
 					for(int i = 0; i < 2; ++i){
 						if(count[i] > 0){
 							align[i][count[i]-1] += 1;
-							valuable_space[i][idx] = valuable_space[i][idx] < count[i] ? count[i] : valuable_space[i][idx];
-							if(prev_space >= 0){
-								valuable_space[i][prev_space] = valuable_space[i][prev_space] < count[i] ? count[i] : valuable_space[i][prev_space];
+							tmp_vspaces[i][offset] = tmp_vspaces[i][offset] < count[i] ? count[i] : tmp_vspaces[i][offset];
+
+							if(prev_offset[i] >= 0){
+								int const po = prev_offset[i];
+								tmp_vspaces[i][po] = tmp_vspaces[i][po] < count[i] ? count[i] : tmp_vspaces[i][po];
 							}
 						}
 					}
 				}
 				count[0] = count[1] = 0;
-				prev_space = idx;
+				prev_offset[0] = prev_offset[1] = offset;
 			}
 			else if(prev_val == 0){
 				count[0] = count[1] = 1;
@@ -277,10 +383,17 @@ static void count_line(int const x_start,
 				/*For both color and dot, check if the previous value match the current value so the serie keep going.*/
 				/*For instance, if prev_val and val indicate the same color or the same type of dot.*/
 				for(int i = 0; i < 2; ++i){
-					if (matching_cells[i][prev_val-1][val-1])
+					if (matching_cells[i][prev_val-1][val-1]){
 						count[i] += 1;
-					else
+					}
+					else{
+						if(prev_offset[i] >= 0){
+							int const po = prev_offset[i];
+							tmp_vspaces[i][po] = tmp_vspaces[i][po] < count[i] ? count[i] : tmp_vspaces[i][po];
+							prev_offset[i] = -1;
+						}
 						count[i] = 1;
+					}
 					if(count[i] == 4){ //TODO The win_length, but I don't have it here
 						align[i][3] += 1;
 					}
@@ -294,28 +407,40 @@ static void count_line(int const x_start,
 		for(int i = 0; i < 2; ++i){
 			if(count[i] > 0){
 				align[i][count[i]-1] += 1;
-				if(prev_space >= 0){
-					valuable_space[i][prev_space] = valuable_space[i][prev_space] < count[i] ? count[i] : valuable_space[i][prev_space];
+				if(prev_offset[i] >= 0){
+					int const po = prev_offset[i];
+					tmp_vspaces[i][po] = tmp_vspaces[i][po] < count[i] ? count[i] : tmp_vspaces[i][po];
 				}
 			}
 		}
 	}
+	for(int i = 0; i < 2; ++i)
+		for(int j = 0; j < offset; ++j){
+			int const x = x_start + j*x_offset_mul;
+			int const y = y_start + j*y_offset_mul;
+			int const idx = x*shape_board[1] + y;
+			int const current_vspace = valuable_space[i][idx];
+			valuable_space[i][idx] = current_vspace < tmp_vspaces[i][j] ? tmp_vspaces[i][j] : current_vspace;
+		}
 }
 static PyObject* heuristic(PyObject *dummy, PyObject *args)
 {
-	PyObject *arg_board=NULL;
-	PyObject *npy_board=NULL;
+	PyObject *arg_board=NULL, *arg_weights=NULL;
+	PyObject *npy_board=NULL, *npy_weights=NULL;
+	int next_player;
 
-	if (!PyArg_ParseTuple(args, "O", &arg_board))
+	if (!PyArg_ParseTuple(args, "OOi", &arg_board, &arg_weights, &next_player))
 		return NULL;
 
 	npy_board = PyArray_FROM_OTF(arg_board, NPY_DOUBLE, NPY_IN_ARRAY);
+	npy_weights = PyArray_FROM_OTF(arg_weights, NPY_DOUBLE, NPY_IN_ARRAY);
 
 	double const* board = PyArray_DATA(npy_board);
+	double const* weights = PyArray_DATA(npy_weights);
 
 	npy_intp *shape_board = PyArray_SHAPE(npy_board);
 
-	int valuable_space[2][shape_board[0] * shape_board[1]];
+	int valuable_space[2][96];
 	for(int i = 0; i < 2; ++i)
 		for(int j = 0; j < shape_board[0] * shape_board[1]; ++j)
 			valuable_space[i][j] = 0;
@@ -344,33 +469,65 @@ static PyObject* heuristic(PyObject *dummy, PyObject *args)
 		}
 	}
 
-	int valuable_space_count[2][4] = {0};
+	int valuable_space_count[2][4];
+	int valuable_space_count_avail[2][4];
 	for(int i = 0; i < 2; ++i)
-		for(int j = 0; j < 4; ++j)
+		for(int j = 0; j < 4; ++j){
 			valuable_space_count[i][j]=0;
+			valuable_space_count_avail[i][j]=0;
+		}
 
 	for(int i = 0; i < 2; ++i)
 		for(int y = shape_board[1]-1; y >= 0; --y)
 			for(int x = 0; x < shape_board[0]; ++x){
 				int val = valuable_space[i][x*shape_board[1]+y];
-				if(val > 0)
-					valuable_space_count[i][val- 1] += 1;
+				if(val > 0){
+					if(y - 2 < 0 || board[x*shape_board[1]+y-2] > 0)
+						valuable_space_count_avail[i][val- 1] += 1;
+					else
+						valuable_space_count[i][val- 1] += 1;
+				}
 			}
-	int weight_align[4] = {0, 1, 8, 1000000};
-	int weight_vspace[4] = {0, 1, 8, 100000};
+	/*int weight_align[4] = {0, 3, 10, 1000000};*/
+	/*int weight_vspace[4] = {0, 1, 8, 100000};*/
 //2 vspace à 3 == victoire
+	double* weight_align = weights;
+	double* weight_vspace = weights+4;
+	double* weight_vspace_avail = weights+8;
 
-	int values[2] = {0};
+	
+	for(int y = shape_board[1]-6; y >= 0; --y){
+		for(int x = 0; x < shape_board[0]; ++x){
+			printf("%d", valuable_space[0][x*shape_board[1]+y]);
+		}
+		printf("\n");
+	}
+	for(int i = 0; i < 2; ++i){
+		printf("Weight:\t");
+		for(int x = 0; x < 4; ++x)
+			printf("%d ", align[i][x]);
+		printf("\n");
+		printf("Vspace:\t");
+		for(int x = 0; x < 4; ++x)
+			printf("%d ", valuable_space_count[i][x]);
+		printf("\n");
+		printf("Vspace av:\t");
+		for(int x = 0; x < 4; ++x)
+			printf("%d ", valuable_space_count_avail[i][x]);
+		printf("\n");
+	}
+
+	double values[2] = {0};
 	for(int i = 0; i < 2; ++i){
 		for(int x = 0; x < 4; ++x){
-			values[i] += weight_align[x] * align[i][x];
-		}
-		for(int x = 0; x < 4; ++x){
-			values[i] += weight_vspace[x] * valuable_space_count[i][x];
+			values[i] += weight_align[x] * (double)align[i][x];
+			values[i] += weight_vspace[x] * (double)valuable_space_count[i][x];
+			values[i] += weight_vspace_avail[x] * (double)valuable_space_count_avail[i][x];
 		}
 	}
 
     Py_DECREF(npy_board);
+	Py_DECREF(npy_weights);
 	return PyInt_FromLong(values[0] - values[1]);
 }
 
@@ -386,6 +543,10 @@ static PyMethodDef hello_methods[] = {
         {
                 "histogram", histogram, METH_VARARGS,
                 "A function to build a quick histogram.",
+        },
+        {
+                "check_pos", check_pos, METH_VARARGS,
+                "A quick critical function of the engine.",
         },
         {
                 "check_move", check_move, METH_VARARGS,
