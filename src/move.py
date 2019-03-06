2cd7e60b6ec278ccaba634d385cf6593f2755bd7
Martinou <martin.khannouz@gmail.com>
2019-03-06 11:11:54 -0400

    Change a print in move.

--- src/move.py
+++ src/move.py
@@ -51,7 +51,7 @@ class Move:
             line += " " + str(self.pos_rec[1]+1)
             #This is random because we don't the second position of the card only the bottom left is used so far
             line += " " + chr(self.pos_rec[0] + 65)
-            line += " " + str(self.pos_rec[1]+2)
+            line += " " + str(self.pos_rec[1]+1)
         else:
             line += "0"
         line += " " + str(self.type)
